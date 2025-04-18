1e02c80e715eb1e7eefaac130f12da0ce8e4f6dc
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2025-04-17 17:46:19 -0700

    Apply softmax formula to existing wordmap functions

--- src/generate.ts
+++ src/generate.ts
@@ -1,6 +1,11 @@
 import readline from 'readline';
 
 type wordMap = Record<string, Record<string, number>>;
+type token = {
+    token: string,
+    weight: number,
+};
+
 const wordMap: wordMap = require('../models/wordMap');
 
 const userInput = readline.createInterface({
@@ -9,15 +14,25 @@ const userInput = readline.createInterface({
 });
 
 function getNextWord(word:string, wordMap: wordMap) {
-    const nextLikelyWords = Object.entries(wordMap[word]);
-    // Get weight of all words and random number
-    const totalWeight = nextLikelyWords.map(entry => entry[1]).reduce((acc, cur) => acc += cur);
-    let randomNumber = Math.random() * totalWeight;
+    // Convert words to token data type
+    const nextLikelyWords: token[] = Object.entries(wordMap[word]).map(word => {
+        const newToken: token = {
+            token: word[0],
+            weight: word[1],
+        }
+        return newToken;
+    });
+
+    const softmaxTokenWeights = applySoftmax(nextLikelyWords);
+
+    // Choose entry
+    let randomNumber = Math.random();
+
     // Select number based on random weight
-    for (let [word, weight] of nextLikelyWords) {
+    for (let {token, weight} of softmaxTokenWeights) {
         randomNumber -= weight;
         if (randomNumber <= 0) {
-            return word;
+            return token;
         }
     }
 }
@@ -47,19 +62,24 @@ function generateParagraph(initialWord: string, wordMap: wordMap, max: number) {
 }
 
 // Softmax function
-function applySoftmax(weightedArray: number[], temperature: number = 1): number[] {
-    const max = Math.max(...weightedArray);
-
-    // Return argmax if temperature is zero (prevent divide-by-zero error)
-    if (temperature === 0) {
-        return weightedArray.map(logit => (logit === max ? 1 : 0));
-    }
+function applySoftmax(tokenArray: token[], temperature: number = 1): token[] {
+    const max = Math.max(...tokenArray.map(token => token.weight));
+    const safeTemp = Math.max(temperature, 1e-6); // Prevent divide by zero errors
 
     // Subtracting max value for numerical stability
-    const expNumbers = weightedArray.map(logit => Math.exp((logit - max) / temperature));
+    const weights: number[] = tokenArray.map(logit => logit.weight);
+    const expNumbers = weights.map(weight => Math.exp((weight - max) / safeTemp));
     const denominator = expNumbers.reduce((acc, curr) => acc += curr, 0);
     const softMaxValues = expNumbers.map(expNum => expNum / denominator);
-    return softMaxValues;
+
+    const weightedTokens: token[] = tokenArray.map((token, i) => {
+        return {
+            token: token.token,
+            weight: softMaxValues[i],
+        }
+    })
+
+    return weightedTokens;
 }
 
 function generateText() {
