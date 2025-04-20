e53828f9631448a5ed9f0e1b1eb412d58d1510a8
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2025-04-20 14:38:37 -0700

    Refactor for fs/promises

--- src/train.ts
+++ src/train.ts
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import { readFile, writeFile, appendFile } from 'fs/promises';
 
 // Data structure for individual words for training
 type Corpus = string[][];
@@ -19,7 +19,7 @@ const bookTitles = [
 
 // Constants for tokenization boundaries
 const wordBoundaryToken = '</w>';
-const bookBoundaryToken = '<|sep|>';
+const bookSeparatorToken = '<|sep|>';
 
 // Build corpus from file
 function buildCorpus(content: string): Corpus {
@@ -28,42 +28,51 @@ function buildCorpus(content: string): Corpus {
     // Divide into characters with word boundary at the end
     let corpus = words.map(word => [...word.split(''), wordBoundaryToken]);
     // Add file separator token
-    corpus.push([bookBoundaryToken]);
+    corpus.push([bookSeparatorToken]);
     // Log progress
     console.log(`Built corpus`);
     return corpus;
 }
 
+// Read file and log result
+function openFile(bookTitle: string): string {
+    // Log progress
+    console.log(`Loading ${bookTitle}...`)
+
+    try {
+        const data = readFile(`./data/dickens/${bookTitle}.txt`);
+        // Convert book text to string and return
+        const content = data.toString();
+        return content;
+    } catch (err) {
+        // Error handling if document can't be read
+        throw new Error(`Error reading ${bookTitle}.txt: ${err}`);
+    }
+}
+
+// Append to corpus document
+function appendBookToCorpus(content: string) {
+    // Log progress
+    console.log(`Appending to corpus...`);
+
+    try {
+        appendFile(`./models/corpus.txt`, content);
+    } catch (err) {
+        throw new Error(`Error writing to corpus: ${err}`)
+    }
+}
+
 // If corpus.txt exists, clear it to prevent writing twice
-fs.writeFileSync('./models/corpus.txt', '');
+await writeFile('./models/corpus.txt', '');
 
 // Iterate over all books
 for (const bookTitle of bookTitles) {
     // Load book file
-    fs.readFile(`./data/dickens/${bookTitle}.txt`, (err, data) => {
-        if (err) {
-            // Error handling if document can't be read
-            console.error(`Error reading ${bookTitle}.txt: ${err}`);
-            return;
-        } else {
-            // Log progress
-            console.log(`Loading ${bookTitle}...`)
-            // Convert book text to string
-            const content = data.toString();
-            // Build corpus from document
-            const corpus: Corpus = buildCorpus(content);
-            // Convert datatype of corpus
-            const corpusText = JSON.stringify(corpus);
-
-            // Save corpus to file
-            fs.appendFile(`./models/corpus.txt`, corpusText, (err) => {
-                if (err) {
-                    console.error(`Error writing corpus: ${err}`);
-                } else {
-                    // Log success
-                    console.log(`Saved file ${bookTitle}`);
-                }
-            })
-        }
-    });
+    const content = openFile(bookTitle);
+    // Build corpus from document
+    const corpus: Corpus = buildCorpus(content);
+    // Convert to string
+    const corpusString = corpus.toString();
+    // Write to corpus
+    appendBookToCorpus(corpusString);
 }
