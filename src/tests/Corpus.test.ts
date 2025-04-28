a2c2536eb82ad1ba40b83792f4b38eb21efc7886
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2025-04-28 16:07:55 -0700

    Format with Prettier

--- src/tests/Corpus.test.ts
+++ src/tests/Corpus.test.ts
@@ -1,4 +1,4 @@
-import { Corpus } from "../Corpus";
+import { Corpus } from '../Corpus';
 
 describe('Corpus module', () => {
   it('initializes with empty array', () => {
@@ -9,16 +9,23 @@ describe('Corpus module', () => {
   it('accepts and splits single text correctly', () => {
     const corpus = new Corpus();
     corpus.addText('hello world');
-    expect(corpus.getTexts()).toEqual([['h','e','l','l','o','</w>'], ['w','o','r','l','d','</w>'], ['<|sep|>']]);
+    expect(corpus.getTexts()).toEqual([
+      ['h', 'e', 'l', 'l', 'o', '</w>'],
+      ['w', 'o', 'r', 'l', 'd', '</w>'],
+      ['<|sep|>'],
+    ]);
   });
 
   it('accepts and splits multiple texts correctly with separator tokens', () => {
     const corpus = new Corpus();
     corpus.addText('hello world');
     corpus.addText('goodbye');
-    expect(corpus.getTexts()).toEqual([['h','e','l','l','o','</w>'], ['w','o','r','l','d','</w>'], ['<|sep|>'],['g','o','o','d','b','y','e','</w>'], ['<|sep|>']]);
+    expect(corpus.getTexts()).toEqual([
+      ['h', 'e', 'l', 'l', 'o', '</w>'],
+      ['w', 'o', 'r', 'l', 'd', '</w>'],
+      ['<|sep|>'],
+      ['g', 'o', 'o', 'd', 'b', 'y', 'e', '</w>'],
+      ['<|sep|>'],
+    ]);
   });
-
-
-
-})
+});
