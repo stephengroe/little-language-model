06ca3021f7c78018b467b552a610ebe218e69593
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2025-04-30 13:47:52 -0700

    Update to use IDs instead of tokens
    * Shifted to use associated token IDs for embedding keys
    * Also changed random number to range between -0.5 and 0.5

--- src/Embedder.ts
+++ src/Embedder.ts
@@ -2,29 +2,29 @@ import { Vocab } from './Vocabulary';
 
 export class Embedder {
   // Create vocabulary
-  private embeddings: Map<string, number[]>;
+  private embeddings: Map<number, number[]>;
 
   // Constructor
   constructor(vocabulary: Vocab, dimensions: number) {
-    this.embeddings = new Map<string, number[]>();
-    this.initializeEmbeddings(vocabulary, dimensions);
+    this.embeddings = new Map<number, number[]>();
+    this.initializeVectors(vocabulary, dimensions);
   }
 
   // Initialize embeddings
-  initializeEmbeddings(vocabulary: Vocab, dimensions: number) {
+  initializeVectors(vocabulary: Vocab, dimensions: number) {
     // Iterate over vocabulary
     for (const [token, tokenId] of vocabulary) {
       // Create array of random numbers
-      const embeddingArray = Array.from({ length: dimensions }, () => {
-        return Math.random();
+      const vectorArray = Array.from({ length: dimensions }, () => {
+        return Math.random() - 0.5; // Center around 0
       });
       // Set as new entry in embeddings Map
-      this.embeddings.set(token, embeddingArray);
+      this.embeddings.set(tokenId, vectorArray);
     }
   }
 
   // Get embeddings
-  getEmbeddings(): Map<string, number[]> {
+  getEmbeddings(): Map<number, number[]> {
     return this.embeddings;
   }
 }
