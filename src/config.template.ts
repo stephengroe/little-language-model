c6fc7abb9d54c268b4128e038da5943ceb1db40c
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2025-05-21 11:57:39 -0700

    Replace config file with template
    * Cleaning up repository updates by removing config.ts from
    	Git (finally) and replacing it with config.template.ts

---
+++ src/config.template.ts
@@ -0,0 +1,27 @@
+export const trainingConfig = {
+  outputFolder: './output/embedding-test',
+  embedding: {
+    vocabularySize: 5_000,
+    vectorSize: 64,
+    epochs: 25,
+    learningRate: 0.01,
+    batchSize: 100,
+    sampledEmbeddings: ['Marley</w>', 'Scrooge</w>', 'Christmas</w>', 'I</w>'],
+    randomSamples: 10, // Number of embeddings to sample in console
+    nearestNeighbors: 10,
+    contextWindow: 2, // Word2Vec context words to include
+    inputTextDirectory: './data/dickens/',
+    inputTexts: [
+      'a-christmas-carol.txt',
+      'american-notes.txt',
+      'bleak-house.txt',
+      'david-copperfield.txt',
+      'hard-times.txt',
+      'little-dorrit.txt',
+      'nicholas-nickleby.txt',
+      'our-mutual-friend.txt',
+      'the-old-curiosity-shop.txt',
+      'the-pickwick-papers.txt',
+    ],
+  },
+};
