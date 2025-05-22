3f693d895e43ddeee447390b57b59702b159a26b
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2025-05-22 15:55:40 -0700

    Create DataLoader class
    * This class will handle training data for the NeuralNet

---
+++ src/DataLoader.ts
@@ -0,0 +1,44 @@
+export type TrainingData = {
+  input: number[];
+  target: number[];
+};
+
+export type TrainingBatch = {
+  batchInputs: number[][];
+  batchTargets: number[][];
+};
+
+class DataLoader {
+  private data: TrainingData[];
+
+  constructor(data: TrainingData[]) {
+    this.data = data;
+  }
+
+  // Shuffle array with Fisher-Yates algorithm
+  shuffle(): void {
+    for (let i = this.data.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.data[i], this.data[j]] = [this.data[j], this.data[i]];
+    }
+  }
+
+  batch(batchSize: number, shuffle?: boolean): TrainingBatch[] {
+    if (shuffle) {
+      this.shuffle();
+    }
+
+    const batches: TrainingBatch[] = [];
+    for (let i = 0; i < this.data.length; i += batchSize) {
+      const dataSlice = this.data.slice(i, i + batchSize);
+      const newBatch: TrainingBatch = { batchInputs: [], batchTargets: [] };
+
+      dataSlice.forEach(({ input, target }) => {
+        newBatch.batchInputs.push(input);
+        newBatch.batchTargets.push(target);
+      });
+      batches.push(newBatch);
+    }
+    return batches;
+  }
+}
