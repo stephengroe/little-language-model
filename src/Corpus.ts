c67b795856ae3f5bc30c496feaabe9aad5291129
Stephen Roe <93727098+stephengroe@users.noreply.github.com>
2025-07-25 14:13:40 -0700

    Add back in multiple spaces
    * Commented out code to remove multiple spaces

--- src/Corpus.ts
+++ src/Corpus.ts
@@ -19,29 +19,31 @@ export class Corpus {
       throw new Error('Content for corpus cannot be an empty string');
     }
 
-    // Remove duplicate spaces
-    let revisedContent: string[] = [];
-    let prevChar = '';
+    // // Remove duplicate spaces
+    // let revisedContent: string[] = [];
+    // let prevChar = '';
 
-    for (let char of content) {
-      if (char === ' ' && prevChar === ' ') return;
-      if (char === '\n') {
-        if (prevChar === ' ') return;
-        else char = ' ';
-      }
+    // for (let char of content) {
+    //   if (char === ' ' && prevChar === ' ') return;
+    //   if (char === '\n') {
+    //     if (prevChar === ' ') return;
+    //     else char = ' ';
+    //   }
 
-      revisedContent.push(char);
-    }
+    //   revisedContent.push(char);
+    // }
+
+    const contentWords = content.match(/ ?\p{L}+| ?\p{N}+| ?\p{P}+/gu) || [];
 
     // Divide into words
-    const contentWords = content.split(/\s+/);
+    // const contentWords = content.split(/\s+/);
 
     // Iterate over each word
-    for (const word of contentWords) {
+    for (const word of contentWords!) {
       // Split word into characters
       const splitWord = word.split('');
       // Add end of word token
-      splitWord.push(wordBoundaryToken);
+      // splitWord.push(wordBoundaryToken);
       // Add to text
       this.texts.push(splitWord);
     }
